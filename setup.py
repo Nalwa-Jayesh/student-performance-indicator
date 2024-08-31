cd8ac319bcdafd35a6252597d84762f092f4b12b
Jayesh Nalwa <nalwajayesh97@gmail.com>
2024-09-01 00:01:02 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from typing import List
+from setuptools import setup, find_packages
+
+HYPHEN_E_DOT = "-e ."
+
+def get_packages(file_path: str) -> List[str]:
+    '''
+    :param file_path:
+    :return: This returns a list of package names
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup(
+    name="StudentPerformanceIndicator",
+    version="0.0.1",
+    author="Jayesh",
+    author_email="nalwajayesh97@gmail.com",
+    packages=find_packages(),
+    install_requires=get_packages("requirements.txt"),
+)
